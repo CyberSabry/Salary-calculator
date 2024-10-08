1ea2c28f7beb031bf42ecbebdea33f25c410cf24
Sabry <sabryefrin19@gmail.com>
2024-10-08 21:16:54 +0300

    v2.1 | Color theme thing optimized.

--- main.js
+++ main.js
@@ -63,170 +63,6 @@ const Elements = {
     }
 }
 
-const Themes = {
-    
-    original: {
-        
-        background: 'hsl(180, 100%, 25%)',
-        main: 'hsl(0, 0%, 78%)', 
-        accent: 'hsl(243, 100%, 26%)', 
-        lightText: 'hsl(0, 0%, 100%)', 
-        darkText: 'hsl(0, 0%, 0%)', 
-        lightBorder: 'hsl(0, 0%, 87%)', 
-        darkBorder: 'hsl(0, 0%, 5%)', 
-        lightBoxShadow: 'hsl(0, 0%, 87%)', 
-        darkBoxShadow: 'hsl(120, 0%, 52%)',
-        inputBackground: 'hsl(0, 0%, 95%)',
-
-        windowsLogoTopLeft: 'hsl(0, 100%, 50%)',
-        windowsLogoTopRight: 'hsl(120, 100%, 50%)',
-        windowsLogoBottomLeft: 'hsl(240, 100%, 50%)',
-        windowsLogoBottomRight: 'hsl(60, 100%, 50%)',
-
-        appLogoScreenColor: 'hsl(180, 100%, 25%)',
-        appLogoLightMainParts: 'hsl(0, 0%, 76%)',
-        appLogoDarkMainParts: 'hsl(0, 0%, 51%)',
-        appLogoAccentParts: 'hsl(247, 100%, 25%)',
-        appLogoLightParts: 'hsl(0, 0%, 100%)',
-        appLogoDarkParts: 'hsl(0, 0%, 0%)',
-        appLogoTextColor: 'hsl(0, 0%, 100%)',
-        appLogoInputBackground: 'hsl(0, 0%, 100%)'
-    },
-    modernDark: {
-        
-        background: 'hsl(0, 0%, 0%)',
-        main: 'hsl(240, 15%, 9%)',
-        accent: 'hsl(240, 6%, 30%)',
-        lightText: 'hsl(240, 15%, 9%)',
-        darkText: 'hsl(36, 77%, 47%)',
-        lightBorder: 'hsl(240, 6%, 30%)',
-        darkBorder: 'hsl(0, 0%, 0%)',
-        lightBoxShadow: 'hsl(243, 18%, 20%)',
-        darkBoxShadow: 'hsl(220, 16%, 4%)',
-        inputBackground: 'hsl(240, 5%, 20%)',
-
-        windowsLogoTopLeft: 'hsl(36, 77%, 47%)',
-        windowsLogoTopRight: 'hsl(240, 6%, 30%)',
-        windowsLogoBottomLeft: 'hsl(240, 6%, 30%)',
-        windowsLogoBottomRight: 'hsl(36, 77%, 47%)',
-
-        appLogoScreenColor: 'hsl(0, 0%, 0%)',
-        appLogoLightMainParts: 'hsl(220, 16%, 24%)',
-        appLogoDarkMainParts: 'hsl(240, 15%, 19%)',
-        appLogoAccentParts: 'hsl(240, 6%, 30%)',
-        appLogoLightParts: 'hsl(240, 6%, 30%)',
-        appLogoDarkParts: 'hsl(240, 15%, 9%)',
-        appLogoTextColor: 'hsl(36, 77%, 47%)',
-        appLogoInputBackground: 'hsl(240, 5%, 20%)'
-    },
-    violetDark: {
-        
-        background: 'hsl(288, 64%, 15%)',
-        main: 'hsl(291, 51%, 26%)',
-        accent: 'hsl(242, 66%, 37%)',
-        lightText: 'hsl(0, 0%, 100%)',
-        darkText: 'hsl(283, 41%, 64%)',
-        lightBorder: 'hsl(292, 26%, 46%)',
-        darkBorder: 'hsl(0, 0%, 0%)',
-        lightBoxShadow: 'hsl(292, 40%, 63%)',
-        darkBoxShadow: 'hsl(292, 54%, 16%)',
-        inputBackground: 'hsl(292, 54%, 20%)',
-
-        windowsLogoTopLeft: 'hsl(283, 41%, 64%)',
-        windowsLogoTopRight: 'hsl(309, 65%, 54%)',
-        windowsLogoBottomLeft: 'hsl(309, 65%, 54%)',
-        windowsLogoBottomRight: 'hsl(283, 41%, 64%)',
-
-        appLogoScreenColor: 'hsl(291, 51%, 26%)',
-        appLogoLightMainParts: 'hsl(292, 26%, 46%)',
-        appLogoDarkMainParts: 'hsl(292, 54%, 16%)',
-        appLogoAccentParts: 'hsl(242, 66%, 37%)',
-        appLogoLightParts: 'hsl(292, 40%, 63%)',
-        appLogoDarkParts: 'hsl(0, 0%, 0%)',
-        appLogoTextColor: 'hsl(283, 41%, 64%)',
-        appLogoInputBackground: 'hsl(292, 54%, 30%)'
-    },
-    ningaTurtles: {
-        
-        background: 'hsl(132, 68%, 18%)',
-        main: 'hsl(106, 73%, 36%)',
-        accent: 'hsl(13, 83%, 50%)',
-        lightText: 'hsl(0, 0%, 100%)',
-        darkText: 'hsl(0, 0%, 0%)',
-        lightBorder: 'hsl(109, 60%, 45%)',
-        darkBorder: 'hsl(0, 0%, 0%)',
-        lightBoxShadow: 'hsl(112, 94%, 68%)',
-        darkBoxShadow: 'hsl(111, 80%, 24%)',
-        inputBackground: 'hsl(106, 68%, 72%)',
-
-        windowsLogoTopLeft: 'hsl(112, 94%, 68%)',
-        windowsLogoTopRight: 'hsl(13, 83%, 50%)',
-        windowsLogoBottomLeft: 'hsl(13, 83%, 50%)',
-        windowsLogoBottomRight: 'hsl(112, 94%, 68%)',
-
-        appLogoScreenColor: 'hsl(132, 68%, 18%)',
-        appLogoLightMainParts: 'hsl(109, 60%, 45%)',
-        appLogoDarkMainParts: 'hsl(111, 80%, 24%)',
-        appLogoAccentParts: 'hsl(13, 83%, 50%)',
-        appLogoLightParts: 'hsl(112, 94%, 68%)',
-        appLogoDarkParts: 'hsl(0, 0%, 0%)',
-        appLogoTextColor: 'hsl(106, 68%, 72%)',
-        appLogoInputBackground: 'hsl(106, 68%, 72%)'
-    },
-    bee: {
-        
-        background: 'hsl(50, 79%, 31%)',
-        main: 'hsl(52, 69%, 51%)',
-        accent: 'hsl(0, 0%, 0%)',
-        lightText: 'hsl(53, 78%, 70%)',
-        darkText: 'hsl(0, 0%, 0%)',
-        lightBorder: 'hsl(51, 68%, 55%)',
-        darkBorder: 'hsl(0, 0%, 0%)',
-        lightBoxShadow: 'hsl(53, 78%, 70%)',
-        darkBoxShadow: 'hsl(50, 80%, 17%)',
-        inputBackground: 'hsl(52, 79%, 72%)',
-
-        windowsLogoTopLeft: 'hsl(52, 69%, 51%)',
-        windowsLogoTopRight: 'hsl(52, 79%, 72%)',
-        windowsLogoBottomLeft: 'hsl(52, 79%, 72%)',
-        windowsLogoBottomRight: 'hsl(52, 69%, 51%)',
-
-        appLogoScreenColor: 'hsl(50, 79%, 31%)',
-        appLogoLightMainParts: 'hsl(53, 78%, 70%)',
-        appLogoDarkMainParts: 'hsl(50, 80%, 20%)',
-        appLogoAccentParts: 'hsl(0, 0%, 0%)',
-        appLogoLightParts: 'hsl(53, 78%, 70%)',
-        appLogoDarkParts: 'hsl(0, 0%, 0%)',
-        appLogoTextColor: 'hsl(0, 0%, 0%)',
-        appLogoInputBackground: 'hsl(52, 79%, 72%)'
-    }
-};
-
-const CSSVariables = [
-    '--background',
-    '--main',
-    '--accent',
-    '--light-text',
-    '--dark-text',
-    '--light-border',
-    '--dark-border',
-    '--light-box-shadow',
-    '--dark-box-shadow',
-    '--input-background',
-    '--windows-logo-top-left',
-    '--windows-logo-top-right',
-    '--windows-logo-bottom-left',
-    '--windows-logo-bottom-right',
-    '--app-logo-screen-color',
-    '--app-logo-light-main-parts',
-    '--app-logo-dark-main-parts',
-    '--app-logo-accent-parts',
-    '--app-logo-light-parts',
-    '--app-logo-dark-parts',
-    '--app-logo-text-color',
-    '--app-logo-input-background'
-]
-
 function validateInputThenClaculate() {
     
     let allValid = true;
@@ -438,31 +274,25 @@ function selectTheme(event) {
     switch (choiceNumber) {
         
         case '0':
-            setTheme(Themes.original);
+            setTheme('default');
             break;
         case '1':
-            setTheme(Themes.modernDark);
+            setTheme('modern-dark');
             break;
         case '2':
-            setTheme(Themes.violetDark);
+            setTheme('violet-dark');
             break;
         case '3':
-            setTheme(Themes.ningaTurtles);
+            setTheme('ninja-turtles');
             break;
         case '4':
-            setTheme(Themes.bee);
+            setTheme('bee');
             break;
     }
 };
 
 function setTheme(theme) {
-
-    const values = Object.values(theme);
-
-    for ( let i = 0; i < CSSVariables.length; i++ ) {
-
-        document.documentElement.style.setProperty(CSSVariables[i], values[i]);
-    }
+    document.body.className = theme;
 };
 
 function makeDraggable(target, trigger, minWidth, minHeight) {
@@ -531,6 +361,8 @@ function displayTime() {
 };
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Set color theme to default:
+    setTheme('default');
     // Action buttons:
     Elements.actionButtons.close.onclick = closeApp;
     Elements.actionButtons.minimize.onclick = minimizeApp;
